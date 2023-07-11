516878add7a37f3e2dfbe275d18708d6311ad030
Chimmys <138540043+Chimmys@users.noreply.github.com>
2023-07-11 08:20:56 +0800

    Revert "Update 更新"
    This reverts commit f9de1c76d3ac2f4aa25ae5de5f130a150092ede9.

--- Assets/A.cs
+++ Assets/A.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 public class A : MonoBehaviour
 {
-   // public Rigidbody2D rb;
-   // public float speed;
-   // public float jumpforce;
     float x=-32;
     // Start is called before the first frame update
     void Start()
@@ -18,29 +15,7 @@ public class A : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         x += Input.GetAxis("Horizontal") * Time.deltaTime * 10;
-         transform.position = new Vector2(x,-140);
-        
-
-        //movement();
-     }
-     /*void movement()
-     {
-         
-         float horizontalmove = Input.GetAxis("Horizontal");
-         float facedircetion = Input.GetAxisRaw("Horizontal");
-
-        if (horizontalmove != 0)
-         {
-             rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, rb.velocity.y);
-         }
-        if (facedircetion != 0) 
-        {
-            rb.velocity = new Vector3(facedircetion, 1, 1);
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = new Vector2(rb.velocity.x , jumpforce * Time.deltaTime);
-        }
-    }*/
+       x += Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+       transform.position = new Vector2(x,-140);
+    }
 }
--- Assets/B.cs
+++ Assets/B.cs
@@ -6,25 +6,13 @@ using UnityEngine.Rendering;
 public class B : MonoBehaviour
 {
     [SerializeField, Header("跳躍高度"), Range(0, 3000)]
-    private float heightJump = 350;
+    private float heightjump = 350;
     [SerializeField, Header("檢查地板尺寸")]
-    private Vector3 v3CheckGroundSize = Vector3.one;
-    [SerializeField, Header("檢查地板位移")]
-    private Vector3 v3CheckGroundOffset;
-    [SerializeField, Header("檢查地板顏色")]
-    private Color colorCheckGround = new Color(1, 0, 0.2f, 0.5f);
-    [SerializeField, Header("檢查地板圖層")]
-    private LayerMask layerCheckGround;
+
+    
     private Animation ani;
     private Rigidbody2D rig;
     private bool clickjump;
-    private bool isGround;
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = colorCheckGround;
-        Gizmos.DrawCube(transform.position + v3CheckGroundOffset, v3CheckGroundSize);
-    }
     private void Awake()
     {
         ani = GetComponent<Animation>();
@@ -33,7 +21,6 @@ public class B : MonoBehaviour
     private void Update()
     {
         JumpKey();
-        CheckGround();
     }
     private void FixedUpdate()
     {
@@ -49,16 +36,10 @@ public class B : MonoBehaviour
     }
     private void JumpForce()
     {
-        if (clickjump && isGround)
+        if (!clickjump)
         {
-            rig.AddForce(new Vector2(0, heightJump));
+            rig.AddForce(new Vector2(0, heightjump));
             clickjump = false;
         }
-    }
-    private void CheckGround()
-    {
-        //Collision2D hit = Physics2D.OverlapBox(transform.position + v3CheckGroundOffset, v3CheckGroundSize, 0, layerCheckGround);
-        //print("碰到的物件 : "+hit.name);
-        //isGround = hit;
     }
 }
